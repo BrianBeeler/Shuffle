20ce07e6b88fe55ecfafac0b433e741ee6812402
Brian Beeler <brianlbeeler@gmail.com>
2021-02-19 16:21:52 -0600

    Bugfix.

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ let  numbersGlobal = [];
 
 function resetDots() {
 
+    debugger;
+
+    count = 0;
+
     let dotsEl = document.getElementById("num-dots")
     let intervalEl = document.getElementById("interval-dots")
     
@@ -19,9 +23,9 @@ function resetDots() {
 
     while (numberOfDots > 0) {
         numbers.push(count);
-        count += +intervalOfDots;
         numberOfDots--;
         document.querySelector("#display").innerHTML+= `<div>${count}</div>`;
+        count += +intervalOfDots;
     }
 
     numbersGlobal = numbers;
@@ -35,8 +39,6 @@ function resetDots() {
 
 function shuffleDots() {
 
-    console.log("Shuffling!");
-
     let newNumbers = []
 
     document.querySelector("#display").innerHTML = "";
@@ -44,15 +46,10 @@ function shuffleDots() {
     while ( numbersGlobal.length ) {
 
         index = Math.floor( Math.random() * numbersGlobal.length );
-
         newNum = numbersGlobal.splice(index, 1);
-
-        
-        document.querySelector("#display").innerHTML+= `<div>${newNum}</div>`;
-
-        
+        document.querySelector("#display").innerHTML+= `<div>${newNum}</div>`;     
         newNumbers.push(newNum);          
-        console.log('While is running!');
+   
     }
 
     numbersGlobal = newNumbers;
